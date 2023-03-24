bdf4a42cc6c7eac5573a8f5e6bb5dc0f617ce2a8
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-24 11:24:22 +0530

    [sanjana] Add :Hash Table

--- DataStruct/Program.cs
+++ DataStruct/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStruct
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+           
+             MyMapNode < string,string> myMapNode = new MyMapNode<string,string>(5);
+             myMapNode.Add("0", "to");
+             myMapNode.Add("1", "be");
+             myMapNode.Add("2", "or");
+             myMapNode.Add("3", "not");
+             myMapNode.Add("4", "to");
+             myMapNode.Add("5", "be");
+             string hash5 = myMapNode.Get("5");
+             Console.WriteLine("5th hash value" + hash5);
+             string hash2 = myMapNode.Get("2");
+             Console.WriteLine("2nd hash value" + hash2);
+
+
+
+
+
+
+
+        }
+    }
+}
